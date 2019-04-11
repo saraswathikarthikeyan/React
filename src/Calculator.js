877a1d87fb89da952f0ccd4f456a788c302c14c3
saraswathi <sara.karthik85@gmail.com>
2019-04-11 10:26:43 +0200

    First commit calculator

---
+++ src/Calculator.js
@@ -0,0 +1,146 @@
+import React, { Component } from 'react';
+import './Calculator.css';
+
+//Calculator component
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        expression:'',
+        answer: '' ,
+        lastClicked:''     
+    };
+    
+    //method called when any button clicked on calculator
+    this.doCalculations = this.doCalculations.bind(this);
+
+    //method called when any button is pressed inside textbox
+    this.isNumber = this.isNumber.bind(this);    
+  }
+
+  //method to add value to the input textbox
+  inputChangedHandler = (event) => {
+     event.target.value = this.state.expression;   
+  }
+  
+  //method called on button click on keypad 
+  doCalculations(btnClicked) {
+    switch(btnClicked) {
+      case '=':
+        try { 
+         //calcultes the expression when equals button is clicked. 
+          let calcAnswer = eval(this.state.expression);         
+          let precision = getPrecision(calcAnswer);
+         
+          if ( +precision >= 7 ) {
+            // truncates the digits after decimal point to 6 digits.
+            calcAnswer = +calcAnswer.toFixed(6);
+          } 
+          this.setState({ lastClicked: 'equal', answer: calcAnswer, expression: calcAnswer });    
+        }
+        catch(err) {     
+          this.setState({  answer: 'err', expression:'err' });  
+        }
+        break;
+      case 'c':
+        //clears the result textbox and label to empty
+        this.setState({  expression: '', answer:'' });
+        break;
+      case 'ce':
+        //delete the last entered digit acts like backspace
+        this.setState({  expression:this.state.expression.slice(0, -1) });
+        break;
+      default:
+        if(this.state.lastClicked === "equal")  { 
+          //checks the last operation and clears the result in textbox for new expression.
+          this.setState({ lastClicked: '', expression: btnClicked });   
+        }
+        else { 
+          //concatinates the entered value to form expression.
+          let question = this.state.expression + btnClicked; 
+          this.setState({ lastClicked: '', expression: question });   
+        } 
+    } 
+  }
+
+  //method called when any button is pressed inside textbox
+  isNumber(evt){  
+    var iKeyCode = (evt.which) ? evt.which : evt.keyCode;
+    if (iKeyCode === 13) {
+      //calls the doCalculations method when enter button is pressed.
+      this.doCalculations("=");    
+    }
+    else if (iKeyCode !== 46 && iKeyCode !== 43 && iKeyCode !== 45 && iKeyCode !== 42 && iKeyCode !== 47
+      && iKeyCode > 31 && (iKeyCode < 48 || iKeyCode > 57))  {  
+      //prevents the alphabets being entered.    
+      evt.preventDefault(); 
+    }
+    else { 
+      //concatinates the entered value to form expression  
+      let question = this.state.expression + evt.key; 
+      this.setState({ lastClicked: '', expression: question });
+    }   
+
+    if(this.state.lastClicked === "equal") { 
+       //checks the last operation and clears the result in textbox for new expression.
+      this.setState({ lastClicked:'', expression: '' });
+    }    
+  }
+
+  //renders calculator component 
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          Calculator
+        </header>
+        <div className="container">
+        <div className="wrapper">
+          <div className="box item1">    
+            <label id="lblresult" className="lblresult">{ this.state.answer }</label>   
+          <div className="box item2">      
+            <input type="text"  id="txtresult" className="txtresult" value = { this.state.expression } 
+            onChange={(e)=>this.inputChangedHandler(e) }
+            onKeyPress =  { e => this.isNumber(e) }   ></input>  
+          </div>
+          </div>
+          <div className="keypad">
+          <div className="box"> <button name="(" onClick= { e => this.doCalculations(e.target.name )  } >(</button></div>
+          <div className="box"> <button name="c" onClick= { e => this.doCalculations(e.target.name ) } >C</button></div>
+          <div className="box"> <button name=")" onClick= { e => this.doCalculations(e.target.name )  } >)</button></div>
+          <div className="box"> <button name="ce" onClick= { e => this.doCalculations(e.target.name )  } >CE</button></div>
+          <div className="box"> <button name="1" onClick= { e => this.doCalculations(e.target.name )  } >1</button></div>
+          <div className="box"> <button name="2" onClick= { e => this.doCalculations(e.target.name )  } >2</button></div>
+          <div className="box"> <button name="3" onClick= { e => this.doCalculations(e.target.name )  } >3</button></div>
+          <div className="box"> <button name="+" onClick= { e => this.doCalculations(e.target.name , this.state)  } >+</button></div>
+          <div className="box"> <button name="4" onClick= { e => this.doCalculations(e.target.name , this.state)  } >4</button></div>
+          <div className="box"> <button name="5" onClick= { e => this.doCalculations(e.target.name , this.state)  } >5</button></div>
+          <div className="box"> <button name="6" onClick= { e => this.doCalculations(e.target.name , this.state)  } >6</button></div>
+          <div className="box"> <button name="-" onClick= { e => this.doCalculations(e.target.name , this.state)  } >-</button></div>
+          <div className="box"> <button name="7" onClick= { e => this.doCalculations(e.target.name , this.state)  } >7</button></div>
+          <div className="box"> <button name="8" onClick= { e => this.doCalculations(e.target.name , this.state)  } >8</button></div>
+          <div className="box"> <button name="9" onClick= { e => this.doCalculations(e.target.name , this.state)  } >9</button></div>
+          <div className="box"> <button name="*" onClick= { e => this.doCalculations(e.target.name , this.state)  } >*</button></div>
+          <div className="box"> <button name="." onClick= { e => this.doCalculations(e.target.name , this.state)  } >.</button></div>
+          <div className="box"> <button name="0" onClick= { e => this.doCalculations(e.target.name , this.state)  } >0</button></div>
+          <div className="box"> <button name="=" onClick= { e => this.doCalculations(e.target.name , this.state)  } >=</button></div>
+          <div className="box"> <button name="/" onClick= { e => this.doCalculations(e.target.name , this.state)  } >/</button></div>
+        </div>
+      </div>
+    </div>  
+      </div>
+    );
+  }
+}
+
+ //method checks decimal result and returns the number of digits after decimal.
+function getPrecision (num) {  
+  var precision =0;
+  if(String(num).indexOf(".") !== -1 ) { 
+    var numAsStr = String(num).split(".");  
+    precision = numAsStr[1].length; 
+  }
+  return precision;  
+}
+
+export default App;
---
+++ src/Calculator.test.js
@@ -0,0 +1,250 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import Calculator from './Calculator';
+import Sinon from 'sinon';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<Calculator />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+
+describe("When testing with Enzyme", () => {
+  let wrapper;
+
+  const spy = Sinon.spy(Calculator.prototype, 'doCalculations');
+  beforeAll(()=>{
+    wrapper = shallow(<Calculator />);    
+  });
+  
+  it("checks header", () => {     
+      expect(wrapper.find("header").length).toBe(1);      
+  });
+
+  it("checks number of buttons", () => {    
+    expect(wrapper.find("button").length).toBe(20);    
+  });
+
+  it("renders a textbox ", () => {    
+    expect(wrapper.find("input").length).toBe(1);    
+  });
+
+  it("renders a result label", () => {    
+    expect(wrapper.find("label").length).toBe(1);    
+  });
+
+  it("renders a first button", () => {    
+    expect(wrapper.find('button').first().length).toBe(1);    
+  });
+
+  /* Test for Addition Starts */
+  it("simulates button click (", () => {    
+    wrapper.find('button').first().simulate('click', {target: { name: "(" } } );   
+    expect(spy.called).toBe(true); 
+    expect(wrapper.find("input").props().value).toEqual('(');
+  });
+
+  it("input text box value on button click 1", () => {    
+    wrapper.find('button').at(4).simulate('click', {target: { name: "1" } } ); 
+    expect(wrapper.find("input").props().value).toEqual('(1');
+  });
+
+  it("input text box value on button click +", () => {    
+    wrapper.find('button').at(7).simulate('click', {target: { name: "+" } } ); 
+    expect(wrapper.find("input").props().value).toEqual('(1+');
+  });
+
+  it("input text box value on button click 2", () => {    
+    wrapper.find('button').at(5).simulate('click', {target: { name: "2" } } ); 
+    expect(wrapper.find("input").props().value).toEqual('(1+2');
+  });
+
+  it("input text box value on button click )", () => {    
+    wrapper.find('button').at(2).simulate('click', {target: { name: ")" } } ); 
+    expect(wrapper.find("input").props().value).toEqual('(1+2)');
+  });
+ 
+  it("checks the output in result label when = button is clicked - 3", () => {    
+    wrapper.find('button').at(18).simulate('click', {target: { name: "=" } } );   
+    expect(spy.called).toBe(true); 
+    expect(wrapper.find("label").text()).toEqual('3'); 
+  });
+ /* Test for Addition Ends */
+
+ it("clears text box value on button click c should be empty", () => {    
+  wrapper.find('button').at(1).simulate('click', {target: { name: "c" } } ); 
+  expect(wrapper.find("input").props().value).toEqual('');
+});
+
+  /* Test for Subtraction Starts */
+ 
+  it("input text box value on button click 8", () => {    
+    wrapper.find('button').at(13).simulate('click', {target: { name: "8" } } ); 
+    expect(wrapper.find("input").props().value).toEqual('8');
+  });
+
+  it("input text box value on button click -", () => {    
+    wrapper.find('button').at(7).simulate('click', {target: { name: "-" } } ); 
+    expect(wrapper.find("input").props().value).toEqual('8-');
+  });
+
+  it("input text box value on button click 3", () => {    
+    wrapper.find('button').at(6).simulate('click', {target: { name: "3" } } ); 
+    expect(wrapper.find("input").props().value).toEqual('8-3');
+  });
+ 
+  it("checks the output in result label when = button is clicked - 5", () => {    
+    wrapper.find('button').at(18).simulate('click', {target: { name: "=" } } );   
+    expect(spy.called).toBe(true); 
+    expect(wrapper.find("label").text()).toEqual('5'); 
+  });
+ /* Test for Subtraction Ends */
+
+ it("clears text box value on button click c should be empty", () => {    
+  wrapper.find('button').at(1).simulate('click', {target: { name: "c" } } ); 
+  expect(wrapper.find("input").props().value).toEqual('');
+});
+
+ /* Test for Multiplication Starts */ 
+ it("input text box value on button click 6", () => {    
+  wrapper.find('button').at(10).simulate('click', {target: { name: "6" } } ); 
+  expect(wrapper.find("input").props().value).toEqual('6');
+});
+
+it("input text box value on button click *", () => {    
+  wrapper.find('button').at(15).simulate('click', {target: { name: "*" } } ); 
+  expect(wrapper.find("input").props().value).toEqual('6*');
+});
+
+it("input text box value on button click 1", () => {    
+  wrapper.find('button').at(4).simulate('click', {target: { name: "1" } } ); 
+  expect(wrapper.find("input").props().value).toEqual('6*1');
+});
+
+it("input text box value on button click 0", () => {    
+  wrapper.find('button').at(17).simulate('click', {target: { name: "0" } } ); 
+  expect(wrapper.find("input").props().value).toEqual('6*10');
+});
+
+it("checks the output in result label when = button is clicked - 60", () => {    
+  wrapper.find('button').at(18).simulate('click', {target: { name: "=" } } );   
+  expect(spy.called).toBe(true); 
+  expect(wrapper.find("label").text()).toEqual('60'); 
+});
+/* Test for Multiplication Ends */
+
+it("checks text box value on button click c should be empty", () => {    
+  wrapper.find('button').at(1).simulate('click', {target: { name: "c" } } ); 
+  expect(wrapper.find("input").props().value).toEqual('');
+});
+
+ /* Test for Division Starts */ 
+ it("input text box value on button click 1", () => {    
+  wrapper.find('button').at(4).simulate('click', {target: { name: "1" } } ); 
+  expect(wrapper.find("input").props().value).toEqual('1');
+});
+ it("input text box value on button click 6", () => {    
+  wrapper.find('button').at(10).simulate('click', {target: { name: "6" } } ); 
+  expect(wrapper.find("input").props().value).toEqual('16');
+});
+
+it("input text box value on button click /", () => {    
+  wrapper.find('button').at(19).simulate('click', {target: { name: "/" } } ); 
+  expect(wrapper.find("input").props().value).toEqual('16/');
+});
+
+it("input text box value on button click 4", () => {    
+  wrapper.find('button').at(8).simulate('click', {target: { name: "4" } } ); 
+  expect(wrapper.find("input").props().value).toEqual('16/4');
+});
+
+it("checks the result label when = button is clicked - 4", () => {    
+  wrapper.find('button').at(18).simulate('click', {target: { name: "=" } } );   
+  expect(spy.called).toBe(true); 
+  expect(wrapper.find("label").text()).toEqual('4'); 
+});
+/* Test for Division Ends */
+
+it("checks text box value on button click c should be empty", () => {    
+  wrapper.find('button').at(1).simulate('click', {target: { name: "c" } } ); 
+  expect(wrapper.find("input").props().value).toEqual('');
+});
+
+/* Test for backspace 'ce' starts */
+it("input text box value on button click 1", () => {    
+  wrapper.find('button').at(4).simulate('click', {target: { name: "1" } } ); 
+  expect(wrapper.find("input").props().value).toEqual('1');
+});
+ it("input text box value on button click 6", () => {    
+  wrapper.find('button').at(10).simulate('click', {target: { name: "6" } } ); 
+  expect(wrapper.find("input").props().value).toEqual('16');
+});
+
+it("input text box value on button click /", () => {    
+  wrapper.find('button').at(19).simulate('click', {target: { name: "/" } } ); 
+  expect(wrapper.find("input").props().value).toEqual('16/');
+});
+
+it("checks text box value on button click ce should delete the last entered value - 16", () => {    
+  wrapper.find('button').at(3).simulate('click', {target: { name: "ce" } } ); 
+  expect(wrapper.find("input").props().value).toEqual('16');
+});
+/* Test for backspace 'ce' ends */
+
+it("checks text box value on button click c should be empty", () => {    
+  wrapper.find('button').at(1).simulate('click', {target: { name: "c" } } ); 
+  expect(wrapper.find("input").props().value).toEqual('');
+});
+
+/* Test for Decimal number starts */
+
+
+it("input text box value on button click 6", () => {    
+  wrapper.find('button').at(10).simulate('click', {target: { name: "6" } } ); 
+  expect(wrapper.find("input").props().value).toEqual('6');
+});
+
+it("input text box value on button click decimal .", () => {    
+  wrapper.find('button').at(16).simulate('click', {target: { name: "." } } ); 
+  expect(wrapper.find("input").props().value).toEqual('6.');
+});
+
+it("input text box value on button click 1", () => {    
+  wrapper.find('button').at(4).simulate('click', {target: { name: "1" } } ); 
+  expect(wrapper.find("input").props().value).toEqual('6.1');
+});
+
+it("input text box value on button click +", () => {    
+  wrapper.find('button').at(7).simulate('click', {target: { name: "+" } } ); 
+  expect(wrapper.find("input").props().value).toEqual('6.1+');
+});
+
+it("input text box value on button click decimal 4", () => {    
+  wrapper.find('button').at(8).simulate('click', {target: { name: "4" } } ); 
+  expect(wrapper.find("input").props().value).toEqual('6.1+4');
+});
+
+it("input text box value on button click decimal .", () => {    
+  wrapper.find('button').at(16).simulate('click', {target: { name: "." } } ); 
+  expect(wrapper.find("input").props().value).toEqual('6.1+4.');
+});
+
+it("input text box value on button click 1", () => {    
+  wrapper.find('button').at(4).simulate('click', {target: { name: "1" } } ); 
+  expect(wrapper.find("input").props().value).toEqual('6.1+4.1');
+});
+
+it("checks display label value - 10.2", () => {    
+  wrapper.find('button').at(18).simulate('click', {target: { name: "=" } } );   
+  expect(spy.called).toBe(true); 
+  expect(wrapper.find("label").text()).toEqual('10.2'); 
+});
+/* Test for Decimal number ends */
+
+});
